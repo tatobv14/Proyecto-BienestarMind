6620ee9704233cf511cc03f471401e36acdda487
tatobv14 <tatobz14@gmail.com>
2025-09-05 23:32:07 -0500

    modificacion welcome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '../css/Css_BienestarMind/dashboard.css';
+import '../css/Css_BienestarMind/Error_404.css';
+import '../css/Css_BienestarMind/Estilo_Formulario.css';
+import '../css/Css_BienestarMind/Login.css';
+import '../css/Css_BienestarMind/Recuperar_contraseña.css';
+import '../css/Css_BienestarMind/style.css';
+
+
